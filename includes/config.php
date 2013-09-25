42bba8267c456c2768a723f378db47a17f103c78
Gautham <gautham.r.w@gmail.com>
2013-09-26 03:59:58 +0530

    Modified Market Time for Testing

--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,10 @@
 	$sqlpass = "BnB123456";
 	$bnbdbase = "nitcfest_bnb";
 	$short_max = 200;
-	$start_time = "09";//'o clock market opens
-        $start_time_min = "18";
-	$end_time = "15";//'o clock  market closes
-	$end_time_min = "30";
+	$start_time = "00";//'o clock market opens
+        $start_time_min = "01";
+	$end_time = "23";//'o clock  market closes
+	$end_time_min = "59";
 	$start_money = 2500000;
 	$max_stock = .1 * $start_money;
 	$short_sell_days = 0;//no of days after which shorted stocks are automatically covered
