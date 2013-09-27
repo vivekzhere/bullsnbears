44a3af520c792c255f22caf2f1e4dcce6bcdcfa8
Gautham <gautham.r.w@gmail.com>
2013-09-27 23:35:49 +0530

    Added a Dirty means of Controlling Config

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,15 @@
 <?php
-	$server = "localhost";
-	$sqlid = "nitcfest_bnbuser";
-	$sqlpass = "BnB123456";
-	$bnbdbase = "nitcfest_bnb";
-	$short_max = 200;
-	$start_time = "00";//'o clock market opens
-        $start_time_min = "01";
-	$end_time = "23";//'o clock  market closes
-	$end_time_min = "59";
-	$start_money = 2500000;
-	$max_stock = .1 * $start_money;
-	$short_sell_days = 0;//no of days after which shorted stocks are automatically covered
+	$config = parse_ini_file('config.ini');
+	$server = $config['server'];
+	$sqlid = $config['sqlid'];
+	$sqlpass = $config['sqlpass'];
+	$bnbdbase = $config['bnbdbase'];
+	$short_max = $config['short_max'];
+	$start_time = $config['start_time'];
+    $start_time_min = $config['start_time_min'];
+	$end_time = $config['end_time'];
+	$end_time_min = $config['end_time_min'];
+	$start_money = $config['start_money'];
+	$max_stock = $config['max_stock'];
+	$short_sell_days = $config['short_sell_days'];
 ?>
