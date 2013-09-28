0b2bd57ae7940784220b21a59352b93e295aa927
Gautham <gautham.r.w@gmail.com>
2013-09-28 12:16:10 +0530

    Fixed a small bug in Lookup.php. Charts work fine now. Updated Chosen jQuery Plugin.

--- lookup.php
+++ lookup.php
@@ -1,73 +1,68 @@
 <?php
  require_once("includes/global.php");
-        if(!isset($_SESSION['username']))
-                header("Location: index.php");
+		if(!isset($_SESSION['username']))
+				header("Location: index.php");
 
-        metadetails();
+		metadetails();
 ?>
 
 
-    <link rel="stylesheet" type="text/css" href="scripts/chosen.css" />
- 
+	<link rel="stylesheet" href="scripts/chosen.min.css">
+	<script type="text/javascript" src="scripts/ticker.js"></script>
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
   
-  <script type="text/javascript" src="scripts/ticker.js"></script>
-    
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js" type="text/javascript"></script>
-  <script src="scripts/chosen.jquery.js" type="text/javascript"></script>
-  <script type="text/javascript"> 
-  $(".chzn-select").chosen(); </script>
-  
-        
+		
    <script type="text/javascript">
-            //setInterval( "updateLookup();", 3000 ); 
-
-            
-            function updateLookup()
-            {
-                   var targeturl = <?php  if(isset($_GET['symbol']))    
-                                                { echo "\"updatelookup.php?symbol=".urlencode($_GET['symbol'])."\""; }
-                                          else 
-                                                { echo "\"updatelookup.php\""; }?>;                            
-                    $.ajax({
-                        url:targeturl
-                       ,dataType: 'HTML'
-                       ,success: function(data, status, xhr){
-                           $('#lookup').html($(data).html());
-                       }
-                    });
-                }
-        </script>
+			function updateLookup()
+			{
+				   var targeturl = <?php  if(isset($_GET['symbol']))	
+												{ echo "\"updatelookup.php?symbol=".urlencode($_GET['symbol'])."\""; }
+										  else 
+												{ echo "\"updatelookup.php\""; }?>;							
+					$.ajax({
+						url:targeturl
+					   ,dataType: 'HTML'
+					   ,success: function(data, status, xhr){
+						   $('#lookup').html($(data).html());
+					   }
+					});
+				}
+		</script>
 </head>
 <body onload="updateLookup()">
 <div id="content">
-        <?php navigation("lookup"); ?>
+		<?php navigation("lookup"); ?>
 <br/>
-        <div id="lookupform">
-                <form method="get" action="lookup.php" name="lookup" onsubmit="updateLookup()"> 
-                        
-                        <select data-placeholder="Choose a Stock..." name="symbol"  style="width:200px; text-align:left;" class="chzn-select">
-                        <option></option>
-                        <?php $out="";
-                                $sql = "select symbol from symbols";
-                                $result = mysql_query($sql) or die(mysql_error());
-                                while($sym = mysql_fetch_assoc($result)){
-                                        $out .= "<option onclick=\"document.forms['lookup'].submit()\"";
-                                        if($_GET['symbol']==$sym['symbol']) $out .= "selected   ";
-                                        $out .= " value=\"{$sym['symbol']}\">{$sym['symbol']}</option>";
-                                        
-                                }
-                                echo $out;
-                        ?>
-                        </select>
-                        <input type="submit" style="float:right;" value="Go"/>
-                <script type="text/javascript"> $(".chzn-select").chosen({no_results_text: "No stocks found"}).change(updateLookup()); </script>
-                </form>
+		<div id="lookupform">
+				<form method="get" action="lookup.php" name="lookup" onsubmit="updateLookup()"> 
+						
+						<select data-placeholder="Choose a Stock..." name="symbol"  style="width:200px; text-align:left;" class="chosen-select">
+						<option></option>
+						<?php $out="";
+								$sql = "select symbol from symbols";
+								$result = mysql_query($sql) or die(mysql_error());
+								while($sym = mysql_fetch_assoc($result)){
+										$out .= "<option onclick=\"document.forms['lookup'].submit()\"";
+										if($_GET['symbol']==$sym['symbol']) $out .= "selected   ";
+										$out .= " value=\"{$sym['symbol']}\">{$sym['symbol']}</option>";
+										
+								}
+								echo $out;
+						?>
+						</select>
+						<input type="submit" style="float:right;" value="Go"/>
+				<script type="text/javascript"> $(".chosen-select").chosen({no_results_text: "No stocks found"}).change(updateLookup()); </script>
+				</form>
 
-        </div><!-- lookup -->
-        <div id="lookup">
-        </div>
-        
-        </div><!-- content_main -->
+		</div><!-- lookup -->
+		<div id="lookup">
+		</div>
+		
 </div><!--content-->
+
+
+  <script src="scripts/chosen.jquery.min.js"></script>
+  <script type="text/javascript"> $(".chosen-select").chosen(); </script>
+
 </body>
 </html>
