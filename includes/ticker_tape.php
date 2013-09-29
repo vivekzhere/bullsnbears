f379357e340f9341be5330d863b3b9097e3092a4
Gautham <gautham.r.w@gmail.com>
2013-09-29 05:58:54 +0530

    Rewritten Ticker to improve SQL performance as well as update to latest jQuery

--- includes/ticker_tape.php
+++ includes/ticker_tape.php
@@ -1,42 +1,27 @@
 <div class="tickercontainer">
-<div id="ticker_tape_content" class="mask">
-	<ul id="ticker" class="newsticker">
- 
-            
-            
-            <?php
-		$sql = "select symbol from symbols";
-		$result = mysql_query($sql);
-		$n = mysql_num_rows($result);
-		$sql = "select symbols.symbol, a.value, a.change from symbols, (select * from stockval order by time_stamp desc limit $n) as a where symbols.symbol = a.symbol";
-		$stocks = mysql_query($sql);
-		$out = "";
-		while($stock = mysql_fetch_array($stocks)){
-			$symbol = $stock['symbol'];
-			$value = $stock['value'];
-			$change = $stock['change'];
-			
-			$out .= "<li> <span class = \"symbol\">{$symbol}</span>  <span class = \"value\">".$value."</span>  <span class = \"change\">".addarrow($change)."</span> </li>";
-		}
-		echo $out;
-	?>
-
-           
+	<div id="ticker_tape_content" class="mask">
+		<ul id="ticker" class="newsticker">
+ 			<?php
+				$sql = "select `symbol`, `value`, `change` from `stockval` order by `time_stamp` desc";
+				$stocks = mysql_query($sql);
+				$out = "";
+				while ($stock = mysql_fetch_array($stocks)) {
+					$symbol = $stock['symbol'];
+					$value = $stock['value'];
+					$change = $stock['change'];
+					$out .= '<li><span class="symbol">'.$symbol.'</span>  <span class="value">'.$value.'</span>  <span class="change">'.addarrow($change).'</span></li>';
+				}
+				echo $out;
+			?>
         </ul>
-        
-</div>
+	</div>
 </div>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="scripts/jquery.ticker.js" ></script>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="scripts/jquery.ticker.js" ></script>
 
 <script type="text/javascript">
 $(document).ready(function() {
-    
-    jQuery('#ticker').webTicker();
+	jQuery('#ticker').webTicker();
 });
- 
- 
- 
 </script>
-
