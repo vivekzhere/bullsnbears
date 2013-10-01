3fef4154f5552b309a5148a0935fc8214b0116dc
Gautham <gautham.r.w@gmail.com>
2013-10-01 21:41:24 +0530

    Cleared Facebook. Testing Branching

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once("includes/global.php");		
 	if(isset($_SESSION['username'])) header("Location: home.php");
-	if($_ISSET['error_reason'])     header('Location: index.php');
-	elseif($_GET['state'] =="8ad4c82bdbf012cf77c6538f5a976279")	header('Location: fb-login.php?key=sasjkhaF_ndSsjkan');
 ?>
 
 <!DOCTYPE html>
@@ -19,30 +17,6 @@ require_once("includes/global.php");
 </head>
 
 <body>
-	<div id="fb-root"></div>
-
-	<!-- Facebook Javascript SDK -->
-	<script>
-	  window.fbAsyncInit = function() {
-		FB.init({
-		  appId	  : '106257179526701', // App ID
-		  status	 : true, // check login status
-		  cookie	 : true, // enable cookies to allow the server to access the session
-		  xfbml	  : true  // parse XFBML
-		});
-
-		// Additional initialization code here
-	  };
-
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=106257179526701";;
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-	</script>
-
 	<div id="content">
 		<div id="banner">
 		</div><!-- banner -->
@@ -56,14 +30,7 @@ js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=106257179526701";;
 				<div id="sponsorad">
 				</div>
 			</div>
-			<div id="fb" style="height: 300px;">
-				<br/><img src = "images/tathva200.PNG"><br/><br/>
-				<h3>Visit <a href = "http://tathva.org">Tathva '13 </h3></a>
-				<br/><br/>
-					<div class="fb-like" data-href="https://www.facebook.com/tathva" data-width="450" data-show-faces="true" data-send="false"></div>
-				<br/><br/>
-				<a href="https://www.facebook.com/dialog/oauth/?client_id=106257179526701&redirect_uri=http://bullsnbears.tathva.org/index.php&state=8ad4c82bdbf012cf77c6538f5a976279&scope=email&display=popup"><div id="loginbutton" style="width=100px;height=20px;"></div></			</div><!-- fb box -->
-		</div><!-- content_main -->
+		</div>
 	</div>
 </body>
 </html>
