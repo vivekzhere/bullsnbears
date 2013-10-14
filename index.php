11bfe97790931b425494fdd3ae3b56024e9ff126
Gautham <gautham.r.w@gmail.com>
2013-10-14 12:34:04 +0530

    Facebook Login Cleaned. Index.php is now stylish & Sassy.

--- index.php
+++ index.php
@@ -1,37 +1,70 @@
 <?php
 require_once("includes/global.php");		
-	if(isset($_SESSION['username'])) header("Location: home.php");
+require_once("fb-sdk/facebook.php");
+
+
+	if (session_id() == '') session_start();
+	if (!(isset($_GET['key']) && $_GET['key'] == 'M1112AER') && !(isset($_SESSION['id']) && in_array($_SESSION['id'], $admins))) {
+		if (($debug_status == 2) || ($debug_status == 1 && $access_status == 0)) header("Location: testing.html") && die();
+		elseif (isset($_SESSION['id'])) header("Location: home.php") && die();
+	} else header("Location: home.php");
+
+	$facebook = new Facebook($fbArray);
+	$user = $facebook->getUser();
+	if ($user) {
+  		try {
+    		$user_profile = $facebook->api('/me');
+  		} catch (FacebookApiException $e) {
+    		error_log($e);
+    		$user = null;
+  		}
+	}
+	if ($user) {
+		$fql =   "select name, pic_square, email from user where uid='$user'";
+		$access_token = $facebook->getAccessToken();
+		$param  =   array('method'     => 'fql.query', 'query'     => $fql, 'access_token' => $access_token , 'callback'    => '');
+		$fqlResult   =   $facebook->api($param);
+		$fbinfo = array('id' => $user, 'name' => $fqlResult[0]['name'], 'email' => $fqlResult[0]['email'], 'picurl' => $fqlResult[0]['pic_square']);
+		$_SESSION['id'] = $fbinfo['id'];
+		$_SESSION['name'] = $fbinfo['name'];
+		$_SESSION['picurl'] = $fbinfo['picurl'];
+		$result = $mysqli->query("SELECT * FROM `player` WHERE `id` = '{$fbinfo['id']}'");
+		if (!$result) echo $mysqli->$error;
+		if ($result->num_rows == 0) {
+			$statement = $mysqli->prepare("INSERT INTO `player` (`id`, `name`, `liq_cash`, `market_val`, `rank`, `short_val`, `day_worth`, `week_worth`, `email`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			$temp = 0;
+			$statement->bind_param('dsddiddds', $fbinfo['id'], $fbinfo['name'], $start_money, $temp, $temp, $temp, $start_money, $start_money, $fbinfo['email']);
+			$statement->execute();
+			echo $mysqli->error;
+			$_SESSION['liq_cash'] = $start_money;
+			$_SESSION['market_val'] =0;
+			$_SESSION['rank']= 0;
+		} else {
+			$row = $result->fetch_assoc();
+			$_SESSION['liq_cash'] = $row['liq_cash'];
+			$_SESSION['market_val'] =  $row['market_val'];
+			$_SESSION['rank']= $row['rank'];
+		}
+		$mysqli->close();
+		header("Location: home.php");
+		die();
+	} else $loginUrl = $facebook->getLoginUrl();
+	metadetails("index");
 ?>
 
-<!DOCTYPE html>
-<html>	
-<head>
-	<title>Bulls n Bears - Organized by Tathva 2013 </title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="description" content="Virtual Stock Market" />
-	<meta name="keywords" content="tathva, bulls and bears" />
-	<meta http-equiv="content-language" content="en"/>
-	<link rel="stylesheet" type="text/css" href="stylesheets/frontpage.css" />
-	<link rel="shortcut icon" href="images/logo.jpg" />
-	<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-	<div id="content">
-		<div id="banner">
-		</div><!-- banner -->
-		
-		<div id="content_main">
-			<div id="leftbox">
-				<div id="tathvaad"> 
-				</div>
-				</div>
-			<div id="rightbox">
-				<div id="sponsorad">
-				</div>
-			</div>
-		</div>
+	<?php FacebookJS($appId); ?>
+	<div id="banner"></div>
+	<div id="fb-box-container"><div class="fb-like-box" data-href="https://www.facebook.com/bullsnbearscommunity" data-width="300" data-height="120" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="true" data-show-border="false"></div></div>
+	<div id="login">
+		<img src="images/user.png" id="user-img" alt="User Image" width="150px" height="150px"></img>
+		<div id="login-btn" onclick='window.location.replace("<?php echo $loginUrl ?>")'></div>
+	</div>
+	<div id="tathva-pic">
+		<img id ="img-tathva" alt="Tathva 13" width="240px" height="90px" style="display: block;" src="images/tathva.png"></img>
+		<div id="fb-like" class="fb-like" data-href="http://facebook.com/tathva" data-width="200px" data-height="20px" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
 	</div>
 </body>
 </html>
-
--- logout.php
+++ logout.php
@@ -1,30 +1,11 @@
 <?php
-	require_once("fb-php-sdk/facebook.php");
-	
-	if(isset($_SESSION['username']))
-		header("Location: home.php");
-	
-	// Setting APP Details and initializing API
-	$app_id = '106257179526701';
-	$app_secret = '9ffd564e1becb3b9691be3c9b13a3e26';
-	$app_namespace = 'bullsnbears';
-	$app_url = 'https://apps.facebook.com/' . $app_namespace . '/';
-	$scope = 'email';
-	$facebook = new Facebook( array(
-		                   'appId' => $app_id,
-		                   'secret' => $app_secret,
-		                 ));
-		                
+require_once("includes/global.php");
+require_once("fb-sdk/facebook.php");
+
+	if (!isset($_SESSION['id'])) header("Location: index.php") && die();
+	$facebook = new Facebook($fbArray);
 	$facebook->destroySession();
 	$_SESSION=array();
-	if(isset($_COOKIE[session_name()])){
-		setcookie(session_name(),'',time()-42000,'/');
-	}
+	if (isset($_COOKIE[session_name()])) setcookie(session_name(),'',time()-42000,'/');
 	session_destroy();
-	header("Location: index.php");
-	
-	
-?>
-
-
-
+	header("Location: index.php") && die();
