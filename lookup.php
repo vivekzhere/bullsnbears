89ee6bdb211359137f9879da86eb0b4a489a9f11
Gautham <gautham.r.w@gmail.com>
2013-10-29 20:13:24 +0530

    AJAX Lookup Stocks with Caching

--- lookup.php
+++ lookup.php
@@ -1,68 +1,74 @@
 <?php
- require_once("includes/global.php");
-		if(!isset($_SESSION['username']))
-				header("Location: index.php");
-
-		metadetails();
+require_once("includes/global.php");
+	if (session_id() == '') session_start();
+	if (!(isset($_SESSION['id']) && in_array($_SESSION['id'], $admins))) {
+		if (($debug_status == 2) || ($debug_status == 1 && $access_status == 0)) header("Location: testing.html") && die();
+		elseif (!isset($_SESSION['id'])) header("Location: index.php") && die();
+	}
+	metadetails();
 ?>
-
-
-	<link rel="stylesheet" href="scripts/chosen.min.css">
-	<script type="text/javascript" src="scripts/ticker.js"></script>
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-  
-		
-   <script type="text/javascript">
-			function updateLookup()
-			{
-				   var targeturl = <?php  if(isset($_GET['symbol']))	
-												{ echo "\"updatelookup.php?symbol=".urlencode($_GET['symbol'])."\""; }
-										  else 
-												{ echo "\"updatelookup.php\""; }?>;							
-					$.ajax({
-						url:targeturl
-					   ,dataType: 'HTML'
-					   ,success: function(data, status, xhr){
-						   $('#lookup').html($(data).html());
-					   }
-					});
-				}
-		</script>
 </head>
-<body onload="updateLookup()">
-<div id="content">
-		<?php navigation("lookup"); ?>
-<br/>
-		<div id="lookupform">
-				<form method="get" action="lookup.php" name="lookup" onsubmit="updateLookup()"> 
-						
-						<select data-placeholder="Choose a Stock..." name="symbol"  style="width:200px; text-align:left;" class="chosen-select">
-						<option></option>
-						<?php $out="";
-								$sql = "select symbol from symbols";
-								$result = mysql_query($sql) or die(mysql_error());
-								while($sym = mysql_fetch_assoc($result)){
-										$out .= "<option onclick=\"document.forms['lookup'].submit()\"";
-										if($_GET['symbol']==$sym['symbol']) $out .= "selected   ";
-										$out .= " value=\"{$sym['symbol']}\">{$sym['symbol']}</option>";
-										
-								}
-								echo $out;
-						?>
-						</select>
-						<input type="submit" style="float:right;" value="Go"/>
-				<script type="text/javascript"> $(".chosen-select").chosen({no_results_text: "No stocks found"}).change(updateLookup()); </script>
-				</form>
+<body onload="Init()">
+	<div id="banner"></div>
+	<?php Menu(); 
 
-		</div><!-- lookup -->
-		<div id="lookup">
-		</div>
+	?>
+	<div id="lookup">
+		<?php
+			$results = $mysqli->query("SELECT * FROM `stocks` ORDER BY `name` ASC");
+			$options = "";
+			$data = "";
+			while ($result = $results->fetch_assoc()) {
+				$options .= "<option value=\"{$result['symbol']}\">{$result['name']}</option>";
+				$data .= "<div id='{$result['symbol']}'>";
+				$data .= "<h2 align='center'>{$result['name']}</h2>";
+				$data .= "<div id='img-{$result['symbol']}' style='background-image: none;'></div>";
+				$data .= "<table style='float: right; margin-top: 30px; margin-right: 20px;'>";
+				$data .= "<tr><td>Value: </td><td>{$result['value']}</td></tr>";
+				$data .= "<tr><td>Change: </td><td>".addarrow($result['change'])."</td></tr>";
+				$data .= "<tr><td>Day High: </td><td>{$result['day_high']}</td></tr>";
+				$data .= "<tr><td>Day Low: </td><td>{$result['day_low']}</td></tr>";
+				$data .= "<tr><td>Year High: </td><td>{$result['week_high']}</td></tr>";
+				$data .= "<tr><td>Year Low: </td><td>{$result['week_low']}</td></tr>";
+				$data .= "</table></div>";
+			}
+			echo '<select id="symbol-select" onchange="ShowValue(this.value)" placeholder="Choose a Stock..." name="symbol" style="width:200px; text-align:left;">';
+			echo $options;
+			echo '</select>';
+		?>	
 		
-</div><!--content-->
+		<div id="symbol" class="box stylebg">
+		</div>
+	</div>
+	<div id="data" style="display: none;">
+	<?php
+		echo $data;
+	?>
+	</div>
 
+	<?php require_once("includes/ticker.php"); AjaxGet(); Load_Anim(); ?>
+	<script>
+		var ReGet = 0;
+		ShowValue(document.getElementById('symbol-select').value);
+		function Init() {
+			setTimeout(function() { ReGet = 1; }, 60000);
+		}
 
-  <script src="scripts/chosen.jquery.min.js"></script>
-  <script type="text/javascript"> $(".chosen-select").chosen(); </script>
+		function ShowValue(a) {
+			if (ReGet == 1) {
+				AjaxGet('updatelookup.php', 'data');
+				ReGet = 2;
+				setTimeout(function() { ReGet = 0; ShowValue(a); Init(); }, 5000);
+			} else {
+				p = document.getElementById('symbol');
+				q = document.getElementById(a);
+				imgS = "style=\"background-image: url('http://ichart.finance.yahoo.com/z?s=" + a.substring(0, 9) + ".NS');\"";
+				q.innerHTML = q.innerHTML.replace("style=\"background-image: none;\"", imgS);
+				p.innerHTML = q.innerHTML;
+				p.innerHTML = p.innerHTML.replace("img-" + a, "img-symbol");
+			}
+		}
+	</script>
 
 </body>
 </html>
--- updatelookup.php
+++ updatelookup.php
@@ -1,45 +1,26 @@
-<?php require_once("includes/global.php");
-	if(!isset($_SESSION['username']))
-		header("Location: index.php");
+<?php
+require_once("includes/global.php");
+	if (session_id() == '') session_start();
+	if (!(isset($_SESSION['id']) && in_array($_SESSION['id'], $admins))) {
+		if (($debug_status == 2) || ($debug_status == 1 && $access_status == 0)) header("Location: testing.html") && die();
+		elseif (!isset($_SESSION['id'])) header("Location: index.php") && die();
+	}
 ?>
 <?php	
-	if(isset($_GET['symbol'])){
-		$flag = 1;
-		$symbol = $_GET['symbol'];
-		$sql="select stockval.symbol, symbols.name, time_stamp, value, stockval.change, day_low, day_high, week_low, week_high from stockval, (select symbol, max(time_stamp) as lt from stockval group  by symbol)  as u, symbols where stockval.symbol=u.symbol  and stockval.time_stamp=u.lt and stockval.symbol = symbols.symbol and stockval.symbol='$symbol' order by stockval.symbol";		
-		$valueset = mysql_query($sql) or die(mysql_error());
-		$values = mysql_fetch_array($valueset);
-		
-		if(mysql_num_rows($valueset)==0) $flag=0;
-		$stock_name = $values['name'];
-		$value = $values['value'];
-		$dhigh = $values['day_high'];
-		$dlow = $values['day_low'];
-		$whigh = $values['week_high'];
-		$wlow = $values['week_low'];
-		$change = $values['change'];
-		$stock_details = "<div id=\"graph\"><img style='width: 100%' src=\"http://ichart.finance.yahoo.com/z?s=".substr(urlencode($symbol), 0, 9).".NS\"></div><br/>";
-		$stock_details .="<div id=\"stock_details\">
-						<h2>$stock_name</h2>
-						<ul>
-						<li>Day high: {$dhigh}</li>
-						<li>Day low: {$dlow}</li>
-						<li>52 Week High: {$whigh}</li>
-						<li>52 Week Low: {$wlow}</li>
-						<li>Price: {$value}</li>
-						<li>Change: ".addarrow($change)."</li></ul>
-						<form method=\"post\" action=\"trade.php?type=Buy\"><input type=\"hidden\" name=\"symbol\" value=\"{$symbol}\"><input type=\"submit\" value=\"Buy\"></form><form method=\"post\" action=\"trade.php?type=Short\"><input type=\"hidden\" name=\"symbol\" value=\"{$symbol}\"><input type=\"submit\" value=\"Short\"></form></div>";
-		//$stock_details .= "<div id=\"graph\"><img src=\"graph.php?symbol={$symbol}\"></div>";
-		  
-		
-	
+	$results = $mysqli->query("SELECT * FROM `stocks` ORDER BY `symbol` ASC");
+	$data = "";
+	while ($result = $results->fetch_assoc()) {
+		$data .= "<div id='{$result['symbol']}'>";
+		$data .= "<h2 align='center'>{$result['name']}</h2>";
+		$data .= "<div id='img-{$result['symbol']}' style='background-image: none;'></div>";
+		$data .= "<table style='float: right; margin-top: 30px; margin-right: 20px;'>";
+		$data .= "<tr><td>Value: </td><td>{$result['value']}</td></tr>";
+		$data .= "<tr><td>Change: </td><td>".addarrow($result['change'])."</td></tr>";
+		$data .= "<tr><td>Day High: </td><td>{$result['day_high']}</td></tr>";
+		$data .= "<tr><td>Day Low: </td><td>{$result['day_low']}</td></tr>";
+		$data .= "<tr><td>Year High: </td><td>{$result['week_high']}</td></tr>";
+		$data .= "<tr><td>Year Low: </td><td>{$result['week_low']}</td></tr>";
+		$data .= "</table></div>";
 	}
-	else 
-	 { $flag=0;
-	   echo "<div></div>";
-	 }
-	 
-	 if($flag==1) echo "<div>$stock_details</div>";
-	 echo "$symbol hai";
-
+	echo $data;
 ?>
