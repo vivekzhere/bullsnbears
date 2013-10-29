01295c37f28b851fdb64646649db572c25f72971
Gautham <gautham.r.w@gmail.com>
2013-10-29 21:07:56 +0530

    Rankings Work. But Styles need to be redone

--- rankings.php
+++ rankings.php
@@ -1,35 +1,25 @@
-<?php require_once("includes/global.php");
+<?php
+require_once("includes/global.php");
 	metadetails();	
  ?>
-	<link href="scripts/jquery.pnotify.default.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body onload="updateRankings()">
 	<div id="content">
-		<?php navigation("rankings"); ?>
-		<br/><br/></br/>
+		<?php if (isset($_SESSION['id'])) Menu(); ?>
+		<br/>
 		<div id="rankings"></div>
-		<button id="rankingsrefresh" class="shinybutton" onclick="updateRankings()">Refresh</button>
+		<button id="rankingsRefresh" class="button btn-green" onclick="updateRankings()">Refresh</button>
 	</div>
 
 	<script type="text/javascript">
 		function updateRankings()
 		{
-			$.ajax({
-				url: 'updaterankings.php'
-			   ,dataType: 'HTML'
-			   ,success: function(data, status, xhr){
-				   	$('#rankings').html($(data).html());
-				   	$.pnotify({ title: 'Hello!', text: 'Page Loaded. You can Refresh the Page after 30s.', animation: 'show',
-						delay: '3000', type: 'success' });
-			   },
-			   failure: function() { $.pnotify({ title: 'Uh Oh!', text: 'Something went wrong! Try again later.', animation: 'show',
-					delay: '3000', type: 'error'  });
-				}
-			});
-   			$("#rankingsrefresh").attr('disabled','disabled');
-			setTimeout(function(){$("#rankingsrefresh").removeAttr('disabled')},30000);
+	    	pr = document.getElementById("rankingsRefresh");
+    		AjaxGet('updaterankings.php', 'rankings');
+			pr.className = pr.className.replace(" btn-green",""); pr.disabled = true;
+	    	setTimeout(function() { pr.className = pr.className + " btn-green"; pr.disabled = false; }, 30000);		    		
 		}
 	</script>
-	<script src="scripts/jquery.pnotify.min.js"></script>
+	<?php AjaxGet(); Load_Anim(); ?>
 </div>
 </body>
--- updaterankings.php
+++ updaterankings.php
@@ -1,36 +1,26 @@
 <?php
-	 require_once("includes/global.php");
-	 if(!isset($_SESSION['username']))
-		header("Location: index.php");
+require_once("includes/global.php");
 ?>
 
 <div>
 <div id="leaderboard">
 <?php		  // Total Ranking
 			$sql = "select id, name,  market_val+liq_cash as tot from player where rank <> 0 order by tot desc limit 20";
-			$resultset = mysql_query($sql) or die(mysql_error());
-			$out = "<h2>Leaderboard</h2><br/><table>
+			$resultset = $mysqli->query($sql) or die(mysql_error());
+			$out = "<div style=\"display: inline-block;\"><h2>Leaderboard</h2><br/><table id=\"overallTable\">
 		<tr>
 			<th>Rank</th><th></th><th>Name</th><th>Net Worth</th>
 		</tr>"; $i = 0;
-			while($player = mysql_fetch_assoc($resultset)){
+			while($player = $resultset->fetch_assoc()){
 				$i = $i+1;
-				$flag = 1;
-				$out .= "\n<tr ";
-				if($flag == 1){
-					$out .= "class=\"altr\"";
-					$flag = 0;
-				}else{
-					$flag = 1;
-				}
-				$out .=">\n";
-				$out .="<td>$i</td>";
-				$player['tot'] = convertcash($player['tot'],"Rs. ");
-				$out .= "<td><img src=\"https://graph.facebook.com/{$player['id']}/picture\" style=\"width:30px;height:30;vertical-align:middle\" /> &nbsp;</td><td style=\"text-align:left\"> {$player['name']}</td>";
+				$out .= "<tr>";
+				$out .="<td>{$i}</td>";
+				$player['tot'] = ininr($player['tot']);
+				$out .= "<td><img src=\"https://graph.facebook.com/{$player['id']}/picture\" style=\"width:30px;height:30px;vertical-align:middle\" /> &nbsp;</td><td style=\"text-align:left\"> {$player['name']}</td>";
 				$out .= "<td>{$player['tot']}</td>";
-				$out .= "\n</tr>";
+				$out .= "</tr>";
 			}
-			$out .= "</table>";
+			$out .= "</table></div><br/><br/>";
 			echo $out;
 		?>
 	
@@ -38,55 +28,39 @@
         <div id="winners" >
 		<?php   // Today's Winners
 			$sql = "select id, name,  market_val+liq_cash-day_worth as tot from player where rank <> 0 order by tot desc limit 10";
-			$resultset = mysql_query($sql) or die(mysql_error());
+			$resultset = $mysqli->query($sql) or die(mysql_error());
 			$out = "<h2>Today's Leaders</h2><br/><table id=\"daywinners\">
 		<tr>
 			<th>Rank</th><th></th><th>Name</th><th>Today's Gain</th>
 		</tr>"; $i = 0;
-			while($player = mysql_fetch_assoc($resultset)){
+			while($player = $resultset->fetch_assoc()) {
 				$i = $i+1;
-				$flag = 1;
-				$out .= "\n<tr ";
-				if($flag == 1){
-					$out .= "class=\"altr\"";
-					$flag = 0;
-				}else{
-					$flag = 1;
-				}
-				$out .=">\n";
-				$out .="<td>$i</td>";
+				$out .= "<tr>";
+				$out .="<td>{$i}</td>";
 				$player['tot'] = ($player['tot']==0)?addarrow(0) : addarrow($player['tot']);
 				$out .= "<td><img src=\"https://graph.facebook.com/{$player['id']}/picture\" style=\"width:30px;height:30;vertical-align:middle\" /> &nbsp;</td><td style=\"text-align:left\">  {$player['name']}</td>";
 				$out .= "<td>{$player['tot']}</td>";
-				$out .= "\n</tr>";
+				$out .= "</tr>";
 			}
-			$out .= "</table><br/><br/><br/><br/>";
+			$out .= "</table><br/><br/>";
 			echo $out;
 		?>
 		
 				<?php  // Weekly Winners
 			$sql = "select id, name,  market_val+liq_cash-week_worth as tot from player where rank <> 0 order by tot desc limit 10";
-			$resultset = mysql_query($sql) or die(mysql_error());
-			$out = "<h2></br></br>This Week's Leaders</h2><br/><table id=\"weekwinners\">
+			$resultset = $mysqli->query($sql) or die(mysql_error());
+			$out = "<h2>This Week's Leaders</h2><br/><table id=\"weekwinners\">
 		<tr>
 			<th>Rank</th><th></th><th>Name</th><th>Week's Gain</th>
 		</tr>"; $i = 0;
-			while($player = mysql_fetch_assoc($resultset)){
+			while($player = $resultset->fetch_assoc()){
 				$i = $i+1;
-				$flag = 1;
-				$out .= "\n<tr ";
-				if($flag == 1){
-					$out .= "class=\"altr\"";
-					$flag = 0;
-				}else{
-					$flag = 1;
-				}
-				$out .=">\n";
-				$out .="<td>$i</td>";
+				$out .= "<tr>";
+				$out .="<td>{$i}</td>";
 				$player['tot'] = ($player['tot']==0)?addarrow(0) : addarrow($player['tot']);			
 				$out .= "<td><img src=\"https://graph.facebook.com/{$player['id']}/picture\" style=\"width:30px;height:30;vertical-align:middle\" /> &nbsp;</td><td style=\"text-align:left\"> {$player['name']}</td>";
 				$out .= "<td>{$player['tot']}</td>";
-				$out .= "\n</tr>";
+				$out .= "</tr>";
 			}
 			$out .= "</table>";
 			echo $out;
