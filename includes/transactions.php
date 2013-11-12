5fe1d0463cc69aad4e0373f48c4f1fc689546436
Gautham <gautham.r.w@gmail.com>
2013-11-12 05:33:47 +0530

    Fix bug in Covering Stocks. Add Support for Scheduled Transactions

--- includes/transactions.php
+++ includes/transactions.php
@@ -1,5 +1,5 @@
 <?php	
-require_once("includes/config.php");
+require_once("config.php");
 
 	function Buy($id, $symbol, $amount, $stock_data, $skey) {
 		global $mysqli;
@@ -10,11 +10,14 @@ require_once("includes/config.php");
 			$p = $mysqli->query("UPDATE `player` SET `rank` = 1, `liq_cash` = `liq_cash` - ".round($amount * $stock_data['value'] * 1.002).", `market_val` = `market_val` + ".round($amount * $stock_data['value'])." WHERE `id` = '{$id}'");
 			if ($p) {
 				$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value`, `skey` ) VALUES ( '{$id}', 'B', '{$symbol}', '{$amount}', '{$stock_data['value']}', '{$skey}' )"); 
-				if (!$p) $mysqli->rollback();
-				else {
-					$mysqli->commit();
-					$err_flag = FALSE;
-				}
+				if ($p) {
+					if ($skey != -1) $p = $mysqli->query("UPDATE `schedule` SET `pend_no_shares` = `pend_no_shares` - {$amount} WHERE `skey` = '{$skey}'"); 
+					if (!$p) $mysqli->rollback();
+					else {
+						$mysqli->commit();
+						$err_flag = FALSE;
+					}
+				} else $mysqli->rollback();
 			} else $mysqli->rollback(); 
 		} else $mysqli->rollback();
 		$mysqli->autocommit(TRUE);
@@ -31,12 +34,15 @@ require_once("includes/config.php");
 		if ($p) {
 			$p = $mysqli->query("UPDATE `player` SET `liq_cash` = `liq_cash` + ".round($amount * $stock_data['value'] * 0.998).", `market_val` = Case When (`market_val` - ".ceil($amount*$stock_data['value']).") < 0 THEN 0 ELSE (`market_val` - ".ceil($amount*$stock_data['value']).") END WHERE `id` = '{$id}'");
 			if ($p) {
-				$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value` ) VALUES ( '{$id}', 'S', '{$symbol}', '{$amount}', '{$stock_data['value']}' )"); 
-				if (!$p) $mysqli->rollback();
-				else {
-					$mysqli->commit();
-					$err_flag = FALSE;
-				}
+				$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value`, `skey` ) VALUES ( '{$id}', 'S', '{$symbol}', '{$amount}', '{$stock_data['value']}', '{$skey}' )"); 
+				if ($p) {
+					if ($skey != -1) $p = $mysqli->query("UPDATE `schedule` SET `pend_no_shares` = `pend_no_shares` - {$amount} WHERE `skey` = '{$skey}'"); 
+					if (!$p) $mysqli->rollback();
+					else {
+						$mysqli->commit();
+						$err_flag = FALSE;
+					}
+				} else $mysqli->rollback();
 			} else $mysqli->rollback(); 
 		} else $mysqli->rollback();
 		$mysqli->autocommit(TRUE);
@@ -47,16 +53,19 @@ require_once("includes/config.php");
 		global $mysqli;
 		$err_flag = TRUE;
 		$mysqli->autocommit(FALSE);
-		$p = $mysqli->query("INSERT INTO `short_sell` VALUES( '{$_SESSION['id']}', '{$symbol}', '{$amount}', '{$stock_data['value']}' ) ON DUPLICATE KEY UPDATE `val` = ((`val` * `amount`) + ".($amount * $stock_data['value'])." ) / (`amount` + ".$amount." ), `amount` = `amount` + ".$amount);
+		$p = $mysqli->query("INSERT INTO `short_sell` VALUES( '{$id}', '{$symbol}', '{$amount}', '{$stock_data['value']}' ) ON DUPLICATE KEY UPDATE `val` = ((`val` * `amount`) + ".($amount * $stock_data['value'])." ) / (`amount` + ".$amount." ), `amount` = `amount` + ".$amount);
 		if ($p) {
-			$p = $mysqli->query("UPDATE `player` SET `rank` = 1, `liq_cash` = `liq_cash` - ".round($amount * $stock_data['value'] * 0.002).", `short_val` = `short_val` + ".round($amount * $stock_data['value'] )." WHERE `id` = '{$_SESSION['id']}'");
+			$p = $mysqli->query("UPDATE `player` SET `rank` = 1, `liq_cash` = `liq_cash` - ".round($amount * $stock_data['value'] * 0.002).", `short_val` = `short_val` + ".round($amount * $stock_data['value'] )." WHERE `id` = '{$id}'");
 			if ($p) {
-				$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value` ) VALUES ( '{$_SESSION['id']}', 'SS', '{$symbol}', '{$amount}', '{$stock_data['value']}' )"); 
-				if (!$p) $mysqli->rollback();
-				else {
-					$mysqli->commit();
-					$err_flag = FALSE;
-				}
+				$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value`, `skey` ) VALUES ( '{$id}', 'SS', '{$symbol}', '{$amount}', '{$stock_data['value']}', '{$skey}' )"); 
+				if ($p) {
+					if ($skey != -1) $p = $mysqli->query("UPDATE `schedule` SET `pend_no_shares` = `pend_no_shares` - {$amount} WHERE `skey` = '{$skey}'"); 
+					if (!$p) $mysqli->rollback();
+					else {
+						$mysqli->commit();
+						$err_flag = FALSE;
+					}
+				} else $mysqli->rollback();
 			} else $mysqli->rollback(); 
 		} else $mysqli->rollback();
 		$mysqli->autocommit(TRUE);
@@ -67,20 +76,23 @@ require_once("includes/config.php");
 		global $mysqli;
 		$err_flag = TRUE;
 		$mysqli->autocommit(FALSE);
-		$x = $mysqli->query("SELECT (amount * val) AS `old` FROM `short_sell` WHERE `id` = '{$_SESSION['id']}' AND `symbol` = '{$symbol}'");
+		$x = $mysqli->query("SELECT val FROM `short_sell` WHERE `id` = '{$id}' AND `symbol` = '{$symbol}'");
 		if ($x) {
 			$x = $x->fetch_assoc();
-			if ($amount != $stock_data['shorted_amount']) $p = $mysqli->query("UPDATE `short_sell` SET `val` = ((`val` * `amount`) - ".($amount * $stock_data['value'])." ) / (`amount` - ".$amount." ), `amount` = `amount` - ".$amount." WHERE `id` = '{$_SESSION['id']}' AND `symbol` = '{$symbol}'");
-			else $p = $mysqli->query("DELETE FROM `short_sell` WHERE `id` = '{$_SESSION['id']}' AND symbol = '{$symbol}'"); 
+			if ($amount != $stock_data['shorted_amount']) $p = $mysqli->query("UPDATE `short_sell` SET `val` = ((`val` * `amount`) - ".($amount * $stock_data['value'])." ) / (`amount` - ".$amount." ), `amount` = `amount` - ".$amount." WHERE `id` = '{$id}' AND `symbol` = '{$symbol}'");
+			else $p = $mysqli->query("DELETE FROM `short_sell` WHERE `id` = '{$id}' AND symbol = '{$symbol}'"); 
 			if ($p) {
-				$p = $mysqli->query("UPDATE `player` SET `liq_cash` = `liq_cash` + ".round($x['old'] - $amount * $stock_data['value'] * 0.998).", `short_val` = Case When (`short_val` - ".ceil($amount * $stock_data['value']).") < 0 THEN 0 ELSE (`short_val` - ".ceil($amount * $stock_data['value']).") END WHERE `id` = '{$_SESSION['id']}'");
+				$p = $mysqli->query("UPDATE `player` SET `liq_cash` = `liq_cash` + ".round(($x['val'] - $stock_data['value'] * 0.998) * $amount).", `short_val` = Case When (`short_val` - ".ceil($amount * $stock_data['value']).") < 0 THEN 0 ELSE (`short_val` - ".ceil($amount * $stock_data['value']).") END WHERE `id` = '{$id}'");
 				if ($p) {
-					$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value` ) VALUES ( '{$_SESSION['id']}', 'C', '{$symbol}', '{$amount}', '{$stock_data['value']}' )"); 
-					if (!$p) $mysqli->rollback();
-					else {
-						$mysqli->commit();
-						$err_flag = FALSE;
-					}
+					$p = $mysqli->query("INSERT INTO `history` ( `p_id`, `t_type`, `symbol`, `amount`, `value`, `skey` ) VALUES ( '{$id}', 'C', '{$symbol}', '{$amount}', '{$stock_data['value']}', '{$skey}' )"); 
+					if ($p) {
+						if ($skey != -1) $p = $mysqli->query("UPDATE `schedule` SET `pend_no_shares` = `pend_no_shares` - {$amount} WHERE `skey` = '{$skey}'"); 
+						if (!$p) $mysqli->rollback();
+						else {
+							$mysqli->commit();
+							$err_flag = FALSE;
+						}
+					} else $mysqli->rollback();
 				} else $mysqli->rollback(); 
 			} else $mysqli->rollback();
 		}
