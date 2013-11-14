372868a82c0ae9487804672862623347f440bcb9
Gautham <gautham.r.w@gmail.com>
2013-11-14 13:56:16 +0530

    Add Remove Stock Function

--- perp/remove_stock.php
+++ perp/remove_stock.php
@@ -1,48 +1,16 @@
 <?php
-	if($_GET['key']=="Ti1lLSK65bds")
-	{
-		require_once("../includes/global.php");
-		
-		// To sell all relcapita shares at today's price 320.05
-			// To remove from bought stock
-			// To add to liq cash = liqcash + amount*320.05*1.002 ; marketval = markvetval -amount*320.05
-		$sql = "select * from bought_stock where symbol='STER.NS'";
-		$results = mysql_query($sql);
-		$i=0;
-		while($result = mysql_fetch_array($results))
-		{
-			echo $i++;
-			$price = $result['avg'];
-			$sql= "select * from player where id = '".$result['id']."'";
-			$playerd = mysql_query($sql) or die(mysql_error());
-			$pdetail = mysql_fetch_array($playerd);
-			$passet=$pdetail['market_val']+$pdetail['liq_cash'];
-			$mval = $pdetail['market_val'];
-			$money=$pdetail['liq_cash'];
-			$tm = strftime("%Y-%m-%d %H:%M:%S", time());
-			$hsql = "insert into history (`t_time`, `p_id`, `t_type`, `symbol`, `skey`, `amount`, `value`, `p_mval`, `p_liqcash`) values('$tm', '".$result['id']."', 's', '".$result['symbol']."', '-1', '".$result['amount']."',  '$price*1.004', '$mval', '$money')";	
-			echo $hsql."<br/>";
-			mysql_query($hsql) or die(mysql_error());
-			
-			$usql = "update player set liq_cash = liq_cash+{$result['amount']}*$price*1.002 where id='{$result['id']}'";
-			echo $usql."<br/>";
-			mysql_query($usql) or die(mysql_error());
-			
-			$usql = "delete from bought_stock where symbol = 'STER.NS' and id = '".$result['id']."'";
-			echo $usql."<br/>";
-			mysql_query($usql) or die(mysql_error());
-			
-			
-		}
-		
-		// To remove all relcapita schedules
-
-		// To remove  ashorted share manually
-		
-		// To add coal india in symbol table
-
-	
-	}
-	else
-	  header('Location:../home.php');
-?>
+require_once("../includes/config.php");
+require_once("../includes/connection.php");
+if ($_GET['key'] == $mainkey && isset($_GET['symbol'])) {
+	$r = $mysqli->query("SELECT value FROM stocks WHERE symbol = '{$_GET['symbol']}'");
+	$r = $r->fetch_assoc();
+	$r = $r['value'];
+	$mysqli->query("INSERT INTO history ( p_id, t_type, symbol, amount, value, skey ) (SELECT id, 'S', '{$_GET['symbol']}', amount, '{$r}' as value, 0 FROM bought_stock WHERE bought_stock.symbol = '{$_GET['symbol']}')");
+	$mysqli->query("INSERT INTO history ( p_id, t_type, symbol, amount, value, skey ) (SELECT id, 'C', '{$_GET['symbol']}', amount, '{$r}' as value, 0 FROM short_sell WHERE short_sell.symbol = '{$_GET['symbol']}')");
+	$mysqli->query("UPDATE player, (SELECT id, amount, '{$r}' as value FROM bought_stock WHERE bought_stock.symbol = '{$_GET['symbol']}') B SET liq_cash = liq_cash + B.amount * B.value * 0.998, market_val = CASE WHEN market_val - B.amount * B.value < 0 THEN 0 ELSE market_val - B.amount * B.value END");
+	$mysqli->query("UPDATE player, (SELECT id, amount, short_sell.val, '{$r}' as value FROM short_sell WHERE short_sell.symbol = '{$_GET['symbol']}') SS SET liq_cash = liq_cash + SS.amount * (SS.val - SS.value * 0.998), short_val = CASE WHEN short_val - SS.amount * SS.value < 0 THEN 0 ELSE short_val - SS.amount * SS.value END");
+	$mysqli->query("DELETE FROM bought_stock WHERE symbol = '{$_GET['symbol']}'");
+	$mysqli->query("DELETE FROM short_sell WHERE symbol = '{$_GET['symbol']}'");
+	$mysqli->query("DELETE FROM schedule WHERE symbol = '{$_GET['symbol']}'");
+	$mysqli->query("DELETE FROM stocks WHERE symbol = '{$_GET['symbol']}'");
+} else header("Location: ../index.php");
